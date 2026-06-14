45284f36e6f7f5f8a04d9cd636439054839e9b7f
Dakota Main <dakotamain@outlook.com>
2026-06-13 18:52:38 -0600

    chore: updated default bind host

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(long, default_value = "localhost")]
+    #[arg(long, default_value = "0.0.0.0")]
     host: String,
     #[arg(short, long, default_value_t = 3000)]
     port: u16,
